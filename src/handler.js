332c3f6ed3a4810357106868a9837ee2bc04bd4b
baloh <balohbaloh@yahoo.co.id>
2022-03-16 21:57:02 +0700

    Adding editBookByIdHandler

--- src/handler.js
+++ src/handler.js
@@ -139,8 +139,81 @@ const getBookByIdHandler = (request, h) => {
   return response;
 };
 
+const editBookByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+  const updatedAt = new Date().toISOString();
+  const finished = readPage === pageCount ? true : false;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    if (!name) {
+      const response = h
+        .response({
+          status: "fail",
+          message: "Gagal memperbarui buku. Mohon isi nama buku",
+        })
+        .code(400);
+      return response;
+    }
+
+    if (readPage > pageCount) {
+      const response = h
+        .response({
+          status: "fail",
+          message:
+            "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+        })
+        .code(400);
+      return response;
+    }
+
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      finished,
+      updatedAt,
+    };
+
+    const response = h
+      .response({
+        status: "success",
+        message: "Buku berhasil diperbarui",
+      })
+      .code(200);
+    return response;
+  }
+
+  const response = h
+    .response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Id tidak ditemukan",
+    })
+    .code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
+  editBookByIdHandler,
 };
